dd621e1fba654e021d43725cf3b36d27300b5f5c
Eling Pramuatmaja <eling@eling.id>
2024-04-12 18:19:21 +0800

    fix: rework everything (second pass) (#10)
    * fix: rewrite the homepage and about me page
    
    * refactor: change css, layout, etc. to improve readability
    
    * style: setup eslint
    
    * fix: change eslint parser ecmaVersion to 2022
    
    * chore: remove unused deps
    
    * refactor: adjust date font weight on blog posts
    
    * ci: run ci on any push or pull_request

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,61 +8,25 @@ module.exports = {
 		"plugin:@typescript-eslint/recommended",
 		"plugin:astro/recommended",
 		"plugin:astro/jsx-a11y-recommended",
-		"plugin:perfectionist/recommended-natural",
+		"plugin:deprecation/recommended",
 		"plugin:regexp/recommended",
-		"plugin:typescript-sort-keys/recommended",
+		"plugin:markdown/recommended-legacy",
 	],
-	ignorePatterns: ["node_modules", "dist"],
 	overrides: [
 		{
-			extends: ["plugin:@typescript-eslint/strict", "plugin:@typescript-eslint/stylistic"],
 			files: ["*.astro"],
 			parser: "astro-eslint-parser",
 			parserOptions: {
 				extraFileExtensions: [".astro"],
 				parser: "@typescript-eslint/parser",
 			},
-			rules: {
-				"astro/jsx-a11y/no-redundant-roles": [
-					"error",
-					{
-						ol: ["list"],
-						ul: ["list"],
-					},
-				],
-			},
-		},
-		{
-			extends: [
-				"plugin:@typescript-eslint/strict-type-checked",
-				"plugin:@typescript-eslint/stylistic-type-checked",
-			],
-			files: ["*.ts", "*.tsx"],
-			rules: {
-				"deprecation/deprecation": "error",
-			},
-		},
-		{
-			extends: ["plugin:markdown/recommended"],
-			files: ["**/*.md"],
-			processor: "markdown/markdown",
 		},
 	],
 	parser: "@typescript-eslint/parser",
-	plugins: [
-		"@typescript-eslint",
-		"astro",
-		"deprecation",
-		"perfectionist",
-		"regexp",
-		"typescript-sort-keys",
-	],
-	root: true,
-	rules: {
-		"@typescript-eslint/no-unused-vars": [
-			"warn",
-			{ ignoreRestSiblings: true, varsIgnorePattern: "Props" },
-		],
-		"@typescript-eslint/no-var-requires": "warn",
+	parserOptions: {
+		ecmaVersion: 2022,
+		sourceType: "module",
+		project: "./tsconfig.json",
 	},
+	root: true,
 };
