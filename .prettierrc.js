9988c9ed9daa746a17a99236862e5de93ddff754
Eling Pramuatmaja <elingpramuatmaja@gmail.com>
2026-02-04 04:41:31 +0800

    chore: use Prettier to format Astro files

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,19 @@
-/** @type {import("@types/prettier").Options} */
+/** @type {import("prettier").Config} */
 export default {
 	printWidth: 100,
 	semi: true,
 	singleQuote: false,
 	tabWidth: 2,
 	useTabs: true,
-	plugins: [
-		// Tailwind plugin must come last for proper sorting in CSS files
-		"prettier-plugin-tailwindcss",
+	plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
+	overrides: [
+		{
+			files: "*.astro",
+			options: {
+				parser: "astro",
+				astroAllowShorthand: true,
+				astroSkipFrontmatter: true,
+			},
+		},
 	],
 };
